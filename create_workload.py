26f65c3e49bf45b1c4cff48d989de3f8b8bc9be3
Tasos Toumazatos <tatoumazatos@gmail.com>
2025-05-01 18:31:48 -0400

    Update create_workload.py

--- create_workload.py
+++ create_workload.py
@@ -3,14 +3,14 @@ import sys
 
 # Usage:
 # $ python3 create_workload.py file_to_save_workload.json seq_length batch_size queries/sample1/* q8_6a505 specific_instances
-# specific_query: eg. q8_6a505, specific_instances: eg. 45
+# specific_query: eg. q8_6a505, specific_instances: eg. 45 (guarantee that q8 appears 45 times in the workload)
 
 target_file = sys.argv[1]
 sequence_length = int(sys.argv[2])
 batch_length = int(sys.argv[3])
 remove_query = int(sys.argv[4])
 query_paths = sys.argv[5:-2]
-specific_query = sys.argv[-2]
-specific_query_instances = int(sys.argv[-1])
+specific_query = sys.argv[-2] # can also be left blank
+specific_query_instances = int(sys.argv[-1]) # can also be left blank (or 0)
 
 batcher.generate_workload(query_paths, sequence_length, batch_length, remove_query, SAVE=True, out_file=target_file, specific_query=specific_query, specific_query_n=specific_query_instances )
