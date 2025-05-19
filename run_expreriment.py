44dec708bfa996ac06dfe97bce00aeea8e4da132
Tasos Toumazatos <tatoumazatos@gmail.com>
2025-05-18 21:37:17 -0400

    Update run_expreriment.py

--- run_expreriment.py
+++ run_expreriment.py
@@ -19,20 +19,20 @@ for num_of_arms in range(MIN_BAO_ARMS_INDEX, MAX_BAO_ARMS):
     print(f"{str(datetime.now())} : Running workload experiment with {num_of_arms} bao arms...")
     
     os.makedirs(f"results/{PATH}/", exist_ok=True)
-    os.makedirs(f"logs/arm_logs_500/", exist_ok=True)
-    f"python3 run_queries_new.py {WORKLOAD_FILE} 1 0 {num_of_arms} logs/arm_logs_500/bao_run_{num_of_arms}_arms_log.json | tee results/arm_results_500/bao_run_{num_of_arms}_arms.txt"
+    os.makedirs(f"logs/arm_logs_1000/", exist_ok=True)
+    f"python3 run_queries_new.py {WORKLOAD_FILE} 1 0 {num_of_arms} logs/arm_logs_1000/bao_run_{num_of_arms}_arms_log.json | tee results/arm_results_1000/bao_run_{num_of_arms}_arms.txt"
 
     os.system("sync")
     print(f"{str(datetime.now())} : Done!")
     sleep(1)
-    os.makedirs(f"db_snapshots/500/{PATH}/", exist_ok=True) # TODO: Delete later
-    os.system(f"cp bao_server/bao.db db_snapshots/500/{PATH}/bao_snapshot_with_{num_of_arms}_arms.db")
+    os.makedirs(f"db_snapshots/1000/{PATH}/", exist_ok=True) # TODO: Delete later
+    os.system(f"cp bao_server/bao.db db_snapshots/1000/{PATH}/bao_snapshot_with_{num_of_arms}_arms.db")
     sleep(5)
-    os.system(f"python3 combiner.py db_snapshots/500/bao_snapshot_with_{num_of_arms}_arms.db results/arm_results_500/bao_run_{num_of_arms}_arms.txt")
+    os.system(f"python3 combiner.py db_snapshots/1000/bao_snapshot_with_{num_of_arms}_arms.db results/arm_results_1000/bao_run_{num_of_arms}_arms.txt")
     sleep(1)
     os.system("cd bao_server && python3 -c 'import storage; storage.flush_tables()'")
     sleep(1)
-    
+
     end = time.time()
     duration_min = (end - start) / 60
 
